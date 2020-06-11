4aa8c7a79fd83d32a86ea6a47a69efb96ebbe6cd
Shelbie Taylor <shelbieltaylor@gmail.com>
2020-06-11 12:53:28 -0500

    added two hookes: useDarkMode and useLocalStorage

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,13 @@
+import useLocalStorage from "./useLocalStorage";
+
+const [someValue, setSomeValue] = useLocalStorage('key')
+
+
+if (initialValue === "true") {
+        body.classList.add("dark-mode")
+    } else {
+        
+            body.classList.remove("dark-mode")
+       
+    }
+return [storedValue, item]
